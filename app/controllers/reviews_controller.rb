9e73e785151e5608e8f71be027380cc004e87521
Max Mitrani <mjmitrani168@gmail.com>
2020-05-13 17:20:16 -0600

    reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def new
     @shelter = Shelter.find(params[:shelter_id])
-  end 
+  end
 
   def create
     @shelter = Shelter.find(params[:shelter_id])
