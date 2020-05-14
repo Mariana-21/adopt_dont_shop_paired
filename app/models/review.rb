3997c046545d009c1bfd6a490756605e6dbe2ae6
Mariana Cid <cidmariana5@gmail.com>
2020-05-14 13:59:45 -0600

    validation for rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :shelter
   validates_presence_of :title, :rating, :content, :picture
+  validates :rating, numericality: {greater_than: 0}
+  validates :rating, numericality: {less_than: 6}
 end
