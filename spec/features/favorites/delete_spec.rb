8f36a851904914fd47d175f9f964a2b567aff4d3
Mariana Cid <cidmariana5@gmail.com>
2020-05-16 17:02:15 -0600

    delete spec added

--- spec/features/favorites/delete_spec.rb
+++ spec/features/favorites/delete_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor" do
-  describe "When I visit my favorites index page" do 
-    it "I can see the pets that I have favorited" do
-      shelter_1 = Shelter.create(name: "Pets Place", address: "341 Bonanza", city:  "Denver", state: "CO", zip: 80127)
+  describe "When I have added pets to my favorites list" do
+    describe "And I visit my favorites page ('/favorites')" do
+      it "I see a link to remove all favorited pets" do 
+        shelter_1 = Shelter.create(name: "Pets Place", address: "341 Bonanza", city:  "Denver", state: "CO", zip: 80127)
       shelter_2 = Shelter.create(name: "San Diego Humane Society", address: "5500 Gaines St", city: "San Diego", state: "CA", zip: "92110")
       pet_1 = Pet.create(name: "Mansy",
                       image: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
@@ -38,18 +39,23 @@ RSpec.describe "As a visitor" do
 
       visit "/favorites" 
 
-      expect(page).to have_link(pet_1.name) 
-      expect(page).to have_link(pet_2.name) 
-      expect(page).to have_link(pet_3.name)
-      expect(page).to have_css("img[src*='#{pet_1.image}']")  
-      expect(page).to have_css("img[src*='#{pet_2.image}']")  
-      expect(page).to have_css("img[src*='#{pet_3.image}']")  
-    end   
-    
-    it "I see text saying that I have no favorited pets" do 
-      visit"/favorites"
+      click_link"Remove All Pets"
 
+      expect(current_path).to eq("/favorites")
       expect(page).to have_content("You haven't favorited any pets. Go find a pet you love!") 
+      expect(page).to_not have_link(pet_1.name) 
+      expect(page).to_not have_link(pet_2.name) 
+      expect(page).to_not have_link(pet_3.name)
+
+      
+      end
     end
   end
 end
+
+
+
+# When I click that link
+# I'm redirected back to the favorites page
+# I see the text saying that I have no favorited pets
+# And the favorites indicator returns to 0
