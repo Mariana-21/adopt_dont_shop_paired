8256be588803cd23bd8b75234e146189219ed342
Max Mitrani <mjmitrani168@gmail.com>
2020-05-17 15:51:17 -0600

    Add favorites with applications functionality

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,22 +7,17 @@ class ApplicationsController < ApplicationController
   def create
     pets = Pet.find(params[:adoptable_pet])
     application = Application.create(application_params)
-    favorites = favorite.pets.map {|pet_id| Pet.find(pet_id)}
-    pets.each do |pet|
-      application.pets << pet
-    end
-    pets.each do |pet|
-      pet.adoptable = false
-    end
-
     if application.save
       flash[:notice] = "Your Application has been submitted for these pets!"
+      pets.each do |pet|
+        application.pets << pet
+      end
       pets.each {|pet| favorite.delete_pet(pet.id.to_s)}
       redirect_to "/favorites"
 
     elsif !application.save
       flash[:notice] = "Application not submitted. Missing one or more of the following fields: Name, Address, City, State, Zip, Phone or Description."
-      render :new
+      redirect_to "/applications/new"
     end
   end
 
