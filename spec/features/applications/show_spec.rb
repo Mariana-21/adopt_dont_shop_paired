742c79e00aa93819f240bdd77452be6709f5c955
Max Mitrani <mjmitrani168@gmail.com>
2020-05-18 10:59:18 -0600

    Add Application show page with pet show page links. Also all included specs.

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  describe "When I have added apet to my favorites list" do
+    it "I can use a link to adopt my favorited pets" do
+      shelter_1 = Shelter.create(name: "Pets Place", address: "341 Bonanza", city:  "Denver", state: "CO", zip: 80127)
+      shelter_2 = Shelter.create(name: "San Diego Humane Society", address: "5500 Gaines St", city: "San Diego", state: "CA", zip: "92110")
+      pet_1 = Pet.create(name: "Mansy",
+                      image: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                      approximate_age:  3,
+                      sex: "Male",
+                      description: "Very Cute Cat!",
+                      shelter_id: shelter_1.id)
+      pet_2 = Pet.create( image: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                        name: "Lanny",
+                        approximate_age:  4,
+                        sex: "Female",
+                        description: "Super Very Cute Cat!",
+                        shelter_id: shelter_1.id)
+      pet_3 = Pet.create( image: "https://dogzone-tcwebsites.netdna-ssl.com/wp-content/uploads/2017/09/chihuahua-names-1.jpg",
+                        name: "Skip", description: "I am very energetic, I love to be around people!", approximate_age: 2, sex: "Male", shelter_id: shelter_2.id)
+      app_1 = Application.create(name: "John", address: "341 Bonanza", city: "Denver", state: "CO", zip: "80127", phone: "303", description: "great app")
+      ApplicationPet.create(application_id: app_1.id, pet_id: pet_1.id)
+      ApplicationPet.create(application_id: app_1.id, pet_id: pet_2.id)
+
+      visit "/applications/#{app_1.id}"
+      expect(page).to have_content(pet_1.name)
+      expect(page).to have_content(pet_2.name)
+      expect(page).to have_content(app_1.name)
+      expect(page).to have_content(app_1.address)
+      expect(page).to have_content(app_1.city)
+      expect(page).to have_content(app_1.state)
+      expect(page).to have_content(app_1.zip)
+      expect(page).to have_content(app_1.phone)
+      expect(page).to have_content(app_1.description)
+
+    end
+  end
+end
