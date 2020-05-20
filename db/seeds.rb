ab0cc87b230e5feedac578ffed3fcd725c9fc623
Mariana Cid <cidmariana5@gmail.com>
2020-05-20 17:42:17 -0600

    refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ApplicationPets.destroy_all
 Application.destroy_all
 Pet.destroy_all
 Review.destroy_all
